dbb3610179e8e98c1c1fabe785621f2343d461c0
artemmax23 <artemmaxzolotarev@gmail.com>
2022-07-13 17:05:31 +0300

    Добавлена функция синхронизации БД и файлового хранилища для minio

--- src/services/mimio_storage.py
+++ src/services/mimio_storage.py
@@ -61,49 +61,29 @@ class MinioFileStorage(StorageInterface):
         return file
 
     def sync(self, all: str) -> list:
-        # result = json.loads(all)
-        # db_paths = [UPLOAD_FOLDER + p['path'] + p['name'] + '.' + p['extension'] for p in result]
-        # cnt = UPLOAD_FOLDER.count('/')
-        #
-        # insert_list = []
-        #
-        # for address, dirs, files in os.walk(UPLOAD_FOLDER):
-        #     for name in files:
-        #         path = os.path.join(address, name)
-        #         path = path.replace('\\', '/')
-        #         address = address.replace('\\', '/')
-        #         if not (path in db_paths):
-        #             filename = name.split('.')
-        #             info = os.stat(path)
-        #             relative = address.split('/', cnt)[cnt]
-        #             if len(relative) != 0:
-        #                 relative = '/' + relative + '/'
-        #             insert_list.append(
-        #                 {'name': filename[0], 'extension': filename[1], 'size': info[6], 'path': relative,
-        #                  'comment': ""})
-        #         else:
-        #             db_paths.remove(path)
-        #
-        # delete_list = []
-        # cnt += 1
-        #
-        # for p in db_paths:
-        #     relative = p.split('/', cnt)
-        #     relative.reverse()
-        #     if relative[0].count('/') != 0:
-        #         fullname, path = relative[0][::-1].split('/', 1)
-        #         path = path[::-1]
-        #         fullname = fullname[::-1]
-        #         path = '/' + path + '/'
-        #     else:
-        #         fullname = relative[0]
-        #         path = '/'
-        #
-        #     if (os.path.exists(UPLOAD_FOLDER + path)) and len(os.listdir(UPLOAD_FOLDER + path)) == 0:
-        #         os.removedirs(UPLOAD_FOLDER + path)
-        #
-        #     name, extension = fullname.split('.', 1)
-        #     delete_list.append({'name': name, 'extension': extension, 'path': path})
-        #
-        # return insert_list, delete_list
-        pass
+        result = json.loads(all)
+        db_paths = [p['path'] for p in result]
+
+        insert_list = []
+
+        for p in self.s3.list_objects(Bucket=self.default_file_bucket)['Contents']:
+            if p['Key'] in db_paths:
+                db_paths.remove(p['Key'])
+            else:
+                fullname = []
+                if p['Key'].count('.') != 0:
+                    fullname = p['Key'].split('.')
+                else:
+                    fullname[0] = p['Key']
+                    fullname[1] = ''
+                insert_list.append(
+                    {'name': fullname[0], 'extension': fullname[1], 'size': p['Size'],
+                     'path': p['Key'], 'comment': ""})
+
+        delete_list = []
+
+        for p in db_paths:
+            file = list(filter(lambda x: x['path'] == p, result))
+            delete_list.append({'name': file[0]['name'], 'extension': file[0]['extension'], 'path': p})
+
+        return insert_list, delete_list
